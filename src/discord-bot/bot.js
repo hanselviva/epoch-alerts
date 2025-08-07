e6f9751ead57526621bcb09629593cc9a11b7b0b
Hansel V <hanselviva@gmail.com>
2025-08-07 13:44:05 -0700

    add start stop alert commands

--- src/discord-bot/bot.js
+++ src/discord-bot/bot.js
@@ -22,7 +22,8 @@ export async function startDiscordBot() {
     if (message.author.bot)
       return;
 
-    if (message.content.toLowerCase() === "status") {
+    // starting alerts
+    if (message.content.toLowerCase() === "!start epoch status alerts") {
       try {
         const results = await Promise.all(
           Object.entries(serverList).map(async ([name, { host, port }]) => {
@@ -35,8 +36,13 @@ export async function startDiscordBot() {
           }),
         );
 
-        const replyText = `**Server Status as of ${getTimeString()}**\n\n${
-          results.map(s => `**${s.name}**: ${s.status}`).join("\n")}`;
+        const replyText = `Server Status as of **${getTimeString()}**\n\n${
+          results.map(s => `**${s.name}**: ${s.status}`).join("\n")}
+   
+This bot will now **start** alerting this channel when there are changes in server status.
+Type STOP to stop getting alerts in this channel.`;
+
+        // ADD logic: add the channel to an external file so keep track of which channel to notify
 
         message.channel.send(replyText);
       }
@@ -44,6 +50,13 @@ export async function startDiscordBot() {
         console.error("Error checking servers:", err);
         message.channel.send("Sorry, something went wrong.");
       }
+    }
+
+    // stopping alert
+    if (message.content.toLowerCase() === "!stop epoch status alerts") {
+      message.channel.send(`This bot will now **stop** alerting this channel when there are changes in server status.`);
+
+      // ADD logic: remove the channel from an external file so keep track of which channel to notify
     }
   });
 
