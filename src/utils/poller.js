c1c880c3628f62aacd87dc0836822326afffce95
Hansel V <hanselviva@gmail.com>
2025-08-08 12:22:02 -0700

    update poller to use net instead of portchecker api

--- src/utils/poller.js
+++ src/utils/poller.js
@@ -1,4 +1,4 @@
-import fetch from "node-fetch";
+import net from "node:net";
 import { getTimeString, serverList } from "../app.js";
 import { notifyDiscordBot } from "../discord-bot/bot.js";
 import { notifyDiscordWebhook } from "../discord-webhook/webhook.js";
@@ -8,24 +8,31 @@ const serverStates = {};
 const WEBHOOK_URL = process.env.DISCORD_WEBHOOK;
 
 export async function checkServer(host, port) {
-  const targetUrl = `https://portchecker.io/api/${host}/${port}`;
-
-  try {
-    const res = await fetch(targetUrl);
-    const rawText = await res.text();
-    const portStatus = rawText.trim(); // "True" or "False"
-    return portStatus === "True";
-  }
-  catch (err) {
-    console.error(`Error checking ${host}:${port}`, err);
-    return false;
-  }
+  return new Promise((resolve) => {
+    const socket = new net.Socket();
+    socket.setTimeout(2000);
+    socket.on("connect", () => {
+      socket.destroy();
+      resolve(true);
+    });
+    socket.on("timeout", () => {
+      socket.destroy();
+      resolve(false);
+    });
+    socket.on("error", () => {
+      socket.destroy();
+      resolve(false);
+    });
+    socket.connect(port, host);
+  });
 }
 
 async function pollServers() {
   for (const [name, { host, port }] of Object.entries(serverList)) {
     const previous = serverStates[name]?.online ?? null;
     const current = await checkServer(host, port);
+
+    previous === null && console.log("--- Polling has started ---");
 
     if (!serverStates[name]) {
       serverStates[name] = {
@@ -38,8 +45,8 @@ async function pollServers() {
     //   await sendUserAlert(name, current);
     }
     else if (current !== previous) {
-      console.log(`**${name}** is now ${current}
-        ${getTimeString()}`);
+      console.log(`**${name}** state changed}
+      to ${current ? "Online" : "Offline"} at ${getTimeString()}`);
 
       serverStates[name].lastOnline = previous;
       serverStates[name].lastChange = getTimeString();
@@ -59,9 +66,8 @@ export async function startServerPolling() {
 
   async function startPolling() {
     await pollServers();
-    setTimeout(startPolling, 30000);
+    setTimeout(startPolling, 10000);
   }
 
-  console.log(`[${getTimeString()}] ----Polling started.----`);
   startPolling();
 }
